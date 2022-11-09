e03342b4206a8dcb0c9c28e82151b1de5e56e80b
Michał Gryczewski <michal12083@outlook.com>
2022-11-09 14:29:47 +0100

    Przygotowanie stylu widiku logowania

--- SystemERP/src/main/java/com/systemerp/systemerp/Main.java
+++ SystemERP/src/main/java/com/systemerp/systemerp/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
 
@@ -15,7 +16,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Parent parent = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
         stage.setScene(new Scene(parent, WIDTH, HEIGHT));
         stage.show();
     }
